0dca6dc69b1dbb631640659a32d801c3528b4b67
Wilson Wong <wongwils@esquel.com>
2018-11-19 15:13:30 +0800

    init

---
+++ src/main/java/com/example/postgresdemo/model/Question.java
@@ -0,0 +1,43 @@
+package com.example.postgresdemo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "questions")
+public class Question extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "question_generator")
+    @SequenceGenerator(
+            name = "question_generator",
+            sequenceName = "question_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String title;
+
+    public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Column(columnDefinition = "text")
+    private String description;
+
+    // Getters and Setters (Omitted for brevity)
+}
